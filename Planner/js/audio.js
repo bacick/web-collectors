43ea7237fe78ef774ed8dda3ffdf9026030d3397
bacick <eshtin@gmail.com>
2021-09-04 21:49:33 +0300

    fix sound truble

--- Planner/js/audio.js
+++ Planner/js/audio.js
@@ -1,6 +1,6 @@
 const sound = require("../media/rickandmortysun.mp3");
 
-let audioTimer = new Audio(sound);
+let audioTimer = new Audio("./media/rickandmortysun.mp3");
 
 export function audio() {
     audioTimer.play();
